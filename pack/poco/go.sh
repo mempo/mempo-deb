72c36203c5e6f9bc6823cae663edb7ed41c5445d
mempo-dev2 <opensource@tigusoft.org>
2013-08-28 12:40:46 +0200

    Fix to poco (libpoco) that makes FMS not usable on debian 7

---
+++ pack/poco/go.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+# mempo-title: Fixed poco library against the lib pcre related bug
+# mempo-prio: 2
+# mempo-why: Program FMS was unusably broken
+# mempo-bugfix-deb: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=671477
+
+echo "Please run as ROOT (if needed): apt-get build-dep poco"
+
+rm -rf build ; mkdir -p build ; cd build
+
+apt-get source poco
+
+cd poco-1.3.6p1
+patch -p 0 < ../../libpcre-8.13.patch
+
+debuild -us -uc -B -j2
+
