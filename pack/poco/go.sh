a17d979b0284a1e4b79068752bd34bd79d768a01
Piotr Kiszka <vyrly@paleale>
2013-09-02 16:34:59 +0200

    deterministic build: poco build always in /tmp/tmpbuild, sha512sum libs check

--- pack/poco/go.sh
+++ pack/poco/go.sh
@@ -7,11 +7,25 @@
 
 echo "Please run as ROOT (if needed): apt-get build-dep poco; apt-get install devscripts faketime"
 
+rm -rf mkdir /tmp/tmpbuild; mkdir -p /tmp/tmpbuild; chmod 700 /tmp/tmpbuild; cp libpcre-8.13.patch /tmp/tmpbuild; cd /tmp/tmpbuild
+
 rm -rf build ; mkdir -p build ; cd build
 
 apt-get source poco
 
 cd poco-1.3.6p1
 patch -p 0 < ../../libpcre-8.13.patch
+faketime "2013-08-28 16:20:26" debuild -us -uc -B -j2
 
-faketime '2013-08-28 16:20:26' debuild -us -uc -B -j2
+cd ..
+FILES=*.deb
+for f in $FILES
+do
+  echo "Extracting $f..."
+  dpkg-deb -x $f out
+done
+
+cd out/usr/lib/
+echo "Checking sha512sum of builded libs"
+sha512sum *.so
+
